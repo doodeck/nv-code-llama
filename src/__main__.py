7ea35972edd04133b710ff55c256d72b7e1b2f82
doodeck <8515450+doodeck@users.noreply.github.com>
2023-12-02 20:45:37 +0000

    Implement alternative request approach with session/get

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,27 @@
 # __main__.py
 
+import argparse
+import sys
 from dotenv import load_dotenv
 from requester import Requester
 
+
 def main() -> None:
+    parser = argparse.ArgumentParser(
+                        prog='NVidiallamaTest',
+                        description='Playing with the NV code llama endpoints',
+                        epilog='Enjoy!')
+    parser.add_argument('-s', '--session', dest='endpoint', action='store_const',
+                        const=Requester.session_endpoint, default=Requester.stream_endpoint,
+                        help='GET using session (default: stream)')
+    args = parser.parse_args()
+
     load_dotenv()  # take environment variables from .env.
-    Requester.ask_endpoint()
+    #  Requester.stream_endpoint()
+    args.endpoint()
 
 
 if __name__ == "__main__":
+    for i, arg in enumerate(sys.argv):
+        print(f"Argument {i:>6}: {arg}")
     main()
--- src/requester.py
+++ src/requester.py
@@ -4,6 +4,7 @@ import requests
 import os
 
 invoke_url = "https://api.nvcf.nvidia.com/v2/nvcf/pexec/functions/df2bee43-fb69-42b9-9ee5-f4eabbeaf3a8"
+fetch_url_format = "https://api.nvcf.nvidia.com/v2/nvcf/pexec/status/"
 
 payload = {
   "messages": [
@@ -15,17 +16,41 @@ payload = {
   "temperature": 0.2,
   "top_p": 0.7,
   "max_tokens": 1024,
-  "stream": True
+  # "stream": True
 }
 
 class Requester:
-    def ask_endpoint(cls) -> None:
+    @classmethod
+    def session_endpoint(cls) -> None:
         API_KEY_REQUIRED_IF_EXECUTING_OUTSIDE_NGC = os.getenv("NV_API_KEY")
         headers = {
             "Authorization": f"Bearer {API_KEY_REQUIRED_IF_EXECUTING_OUTSIDE_NGC}",
-            "accept": "text/event-stream",
-            "content-type": "application/json",
+            "Accept": "application/json",
         }
+        payload["stream"] = False
+
+        # re-use connections
+        session = requests.Session()
+
+        response = session.post(invoke_url, headers=headers, json=payload)
+
+        while response.status_code == 202:
+            request_id = response.headers.get("NVCF-REQID")
+            fetch_url = fetch_url_format + request_id
+            response = session.get(fetch_url, headers=headers)
+
+        response.raise_for_status()
+        response_body = response.json()
+        print(response_body)
+    
+    def stream_endpoint(cls) -> None:
+        API_KEY_REQUIRED_IF_EXECUTING_OUTSIDE_NGC = os.getenv("NV_API_KEY")
+        headers = {
+            "Authorization": f"Bearer {API_KEY_REQUIRED_IF_EXECUTING_OUTSIDE_NGC}",
+            "Accept": "text/event-stream",
+            "Content-Type": "application/json",
+        }
+        payload["stream"] = True
 
         response = requests.post(invoke_url, headers=headers, json=payload, stream=True)
 
@@ -33,4 +58,4 @@ class Requester:
             if line:
                 print(line.decode("utf-8"))
 
-Requester.ask_endpoint = classmethod(Requester.ask_endpoint)
+Requester.stream_endpoint = classmethod(Requester.stream_endpoint)
